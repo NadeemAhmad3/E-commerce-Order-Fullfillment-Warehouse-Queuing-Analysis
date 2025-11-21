4c318b34711cd8597591df74a6a8635e60b33931
Nadeem Ahmad <onedaysuccussfull@gmail.com>
2025-11-21 21:15:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import Features from './components/Features';
+
+import Team from './components/Team';
+
 
 export default function Home() {
   return (
     <main className="bg-[#0a1929] min-h-screen">
       <Navbar />
       <HeroSection />
+      <Features />
+      <Team />
     </main>
   );
 }
