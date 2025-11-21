9cc65fb9c5d269c5017603e62fea954046f5e593
Nadeem Ahmad <onedaysuccussfull@gmail.com>
2025-11-21 19:44:11 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
+// app/page.tsx
 import React from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 
 export default function Home() {
   return (
-    <main className="bg-[#0a1929]">
+    <main className="bg-[#0a1929] min-h-screen">
       <Navbar />
       <HeroSection />
-      {/* Add more sections here later */}
-      {/* <FeaturesSection /> */}
-      {/* <AboutSection /> */}
-      {/* <TeamSection /> */}
-      {/* <Footer /> */}
     </main>
   );
 }
